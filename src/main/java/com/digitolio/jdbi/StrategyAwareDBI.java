c3de6c0f70423ed17047f0fd674384defbf75398
Cemalettin Koc <cem.koc.fwd@gmail.com>
2012-10-22 10:14:12 +0300

    enhancing added for strategy awareness

--- src/main/java/com/digitolio/jdbi/StrategyAwareDBI.java
+++ src/main/java/com/digitolio/jdbi/StrategyAwareDBI.java
@@ -1,38 +1,15 @@
 package com.digitolio.jdbi;
 
-import com.digitolio.jdbi.strategy.TranslatingStrategyAware;
+import com.digitolio.jdbi.strategy.SnakeCaseTranslatingStrategy;
 import org.skife.jdbi.v2.DBI;
-import org.skife.jdbi.v2.tweak.ConnectionFactory;
 
-import javax.sql.DataSource;
-import java.util.Properties;
-
-public class StrategyAwareDBI extends DBI {
+public class StrategyAwareDBI{
 
     public static final String TRANSLATING_STRATEGY = "TRANSLATING_STRATEGY";
 
-    public StrategyAwareDBI(DataSource dataSource, TranslatingStrategyAware translater) {
-        super(dataSource);
-        define(TRANSLATING_STRATEGY,translater);
-    }
-
-    public StrategyAwareDBI(ConnectionFactory connectionFactory, TranslatingStrategyAware translater) {
-        super(connectionFactory);
-        define(TRANSLATING_STRATEGY,translater);
-    }
-
-    public StrategyAwareDBI(String url, TranslatingStrategyAware translater) {
-        super(url);
-        define(TRANSLATING_STRATEGY,translater);
-    }
-
-    public StrategyAwareDBI(String url, Properties props, TranslatingStrategyAware translater) {
-        super(url, props);
-        define(TRANSLATING_STRATEGY,translater);
-    }
-
-    public StrategyAwareDBI(String url, String username, String password, TranslatingStrategyAware translater) {
-        super(url, username, password);
-        define(TRANSLATING_STRATEGY,translater);
+    public static DBI enhanceDBI(DBI dbi){
+        dbi.registerArgumentFactory(new EnumArgumentFactory());
+        dbi.define(StrategyAwareDBI.TRANSLATING_STRATEGY, new SnakeCaseTranslatingStrategy());
+        return dbi;
     }
 }

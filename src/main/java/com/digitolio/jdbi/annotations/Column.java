9ad7282d73aca54cc8f9b4b314049871f5f8083f
Cemalettin Koc <cem.koc.fwd@gmail.com>
2012-11-06 10:39:06 +0200

    column annotation added

---
+++ src/main/java/com/digitolio/jdbi/annotations/Column.java
@@ -0,0 +1,12 @@
+package com.digitolio.jdbi.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Column {
+    boolean nullable() default false;
+}
